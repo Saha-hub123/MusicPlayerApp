e55ed21335d61f73a62a53a7c72dd90d430393c4
faidilzahpar <paidilzahpar@gmail.com>
2025-12-10 15:44:12 +0700

    ui

--- Controllers/MusicController.cs
+++ Controllers/MusicController.cs
@@ -5,28 +5,6 @@ namespace MusicPlayerApp.Controllers
 {
     public class MusicController
     {
-        private readonly AudioPlayerService _player;
-        private readonly LocalMusicService _local;
-
-        public MusicController(AudioPlayerService player, LocalMusicService local)
-        {
-            _player = player;
-            _local = local;
-        }
-
-        public List<Song> LoadSongs(string folderPath)
-        {
-            return _local.GetSongsFromFolder(folderPath);
-        }
-
-        public void Play(Song song)
-        {
-            _player.Play(song.FilePath);
-        }
-
-        public void Stop()
-        {
-            _player.Stop();
-        }
+        
     }
 }
